5d85eb83ff095a6e34e9271cabf66f73bc1ea03c
Anton Hörl <anton.hoerl@gmx.at>
2018-02-19 20:45:31 +0100

    Umsetzen bug corrected again REALLY KILLLLLLLLL?

--- routes/imHausListe.js
+++ routes/imHausListe.js
@@ -189,9 +189,10 @@ module.exports = {
         let zimmernummerValueArray = [];
         let informationElementsString = JSON.stringify(informationElements);
 
+
         if (informationElementsString.indexOf("targetTable") != -1) {
-            return;
-        }
+            console.log("BUG I GONNA KILL YOU !!!!")
+        } else {
 
         if (informationElementsString.indexOf("leftValue") != -1) {
             for (let i = 0; i < informationElements.groups.length; i++) {
@@ -237,7 +238,7 @@ module.exports = {
                     }
                     console.log("occupyTable Update successful");
                 });
-        }
+        }}
 
         setTimeout(function () {
             db.imHausListeTest.find(
